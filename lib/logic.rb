688c81362dbc0d613cf358aea900e3b3cb3ec438
madcido <fabowow@gmail.com>
2019-04-18 16:38:51 -0300

    player class implemented

--- lib/logic.rb
+++ lib/logic.rb
@@ -14,4 +14,45 @@ class Board
   def write (tile, mark)
     @tiles[tile-1] = mark
   end
+
+  def full?
+
+  end
+end
+
+class Player
+  attr_reader :score, :inputs
+
+  def initialize (mark)
+    @mark = mark
+    @score = 0
+    @inputs = []
+  end
+
+  def input (input)
+    @inputs << input
+  end
+end
+
+
+module Checks
+  V_CONDITIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+
+  def valid?
+    case self
+    when (1..9)
+      return true
+    else
+      return false
+    end
+  end
+
+  def victory? (player_inputs)
+    V_CONDITIONS.each { |x| return true if player_inputs.any?(x[0]) && player_inputs.any?(x[1]) && player_inputs.any?(x[2])}
+    false
+  end
+
+  def tie?
+
+  end
 end
